2946f809578096883270fcb2e6405984b4382fef
ghadaichandan39-ctrl <ghadaichandan39@gmail.com>
2026-04-02 21:12:10 +0530

    Time: 60 ms (78.61%), Space: 107.5 MB (48.29%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+      int n=temperatures.length;
+      int[] result=new int[n];
+      Stack<Integer> s=new Stack<>();
+      for(int i=0;i<n;i++){
+        while(!s.isEmpty() && temperatures[i]>temperatures[s.peek()]){
+            int pidx=s.pop();
+            result[pidx]=i-pidx;
+        }
+        s.push(i);
+      }
+      return result;
+
+    }
+}
