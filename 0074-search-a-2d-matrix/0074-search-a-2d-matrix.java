4acf742cd62a4bbf6a61400d7970e5d979e47c0a
ghadaichandan39-ctrl <ghadaichandan39@gmail.com>
2026-04-07 20:54:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (84.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-     HashSet<Integer> map= new HashSet<>();
+    //  HashSet<Integer> map= new HashSet<>();
      for(int i=0;i<matrix.length;i++){
         for(int j=0;j<matrix[i].length;j++){
-            map.add(matrix[i][j]);
+            // map.add(matrix[i][j]);
+            if(matrix[i][j]==target){
+                return true;
+            }
         }
      }
-       for(int k:map){
-        if(k==target){
-            return true;
-        }
-       } 
+    //    for(int k:map){
+    //     if(k==target){
+    //         return true;
+    //     }
+    //    } 
        return false;  
     }
 }
