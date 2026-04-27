9b7d309791239b43c17f3981b4fa6e93467ccc5d
ghadaichandan39-ctrl <ghadaichandan39@gmail.com>
2026-04-27 19:11:34 +0530

    Time: 1 ms (99.97%), Space: 46.6 MB (91.92%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
      int result=0;
+     //in this for loop we use xor opration for this xor opration same double values are zero or single value is store in the result
      for( int i=0; i<nums.length;i++){
         result^=nums[i];
      }
